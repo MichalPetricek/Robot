caf5a1ae0124e629d6e15300596b99174e17f4f7
micha <micha@LAPTOP-9V3QC6UI>
2019-11-15 08:57:15 +0100

    Add project files.

---
+++ Robot/RoboticChassis.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Robot
+{
+    class RoboticChassis :RoboticPart
+    {
+        public void BatteryCapacity()
+        {
+
+        }
+
+        public void RemainingCapacity()
+        {
+
+        }
+
+
+    }
+}
---
+++ Robot/RoboticPart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Robot
+{
+    abstract class RoboticPart
+    {
+        protected int consumption;
+        protected int basicconsumption = 100;
+
+
+    }
+}
